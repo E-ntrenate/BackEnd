3ad605ee4953872c9ae2123e23eaf95a88cb0088
Judirale13 <jramirezle@unal.edu.co>
2022-05-25 23:09:43 -0500

    Formulario_registro

--- src/main/java/com/example/entrenate/config/SecurityConfiguration.java
+++ src/main/java/com/example/entrenate/config/SecurityConfiguration.java
@@ -27,7 +27,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/assets/**", "/styles/**").permitAll()
+                .antMatchers("/registro", "/", "/assets/**", "/styles/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
