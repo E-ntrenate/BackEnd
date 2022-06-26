3552069bdf026d2b3b2756b330044a380edf6fde
Santiago Rodríguez Vallejo <sarodriguezva@unal.edu.co>
2022-06-26 14:33:04 -0500

    Arreglos logout

--- src/main/java/com/example/entrenate/security/SecurityConfiguration.java
+++ src/main/java/com/example/entrenate/security/SecurityConfiguration.java
@@ -53,7 +53,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                     .logoutUrl("/logout")
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
                     .clearAuthentication(true)
                     .invalidateHttpSession(true)
                     .deleteCookies("JSESSIONID", "remember-me")
